4ad978fc78472d1cd5d8776d867549654852ae48
Tatty <tattyklass@gmail.com>
2019-09-13 18:16:39 -0700

    enabled turns, structure overhaul

--- py_pac_po.py
+++ py_pac_po.py
@@ -2,20 +2,35 @@ print("---------------------")
 print("Let's play Py-Pac-Poe")
 print("---------------------")
 
-def init_game():
-    state = {}
+score = {"X": 0, "O": 0, "T":0}
+board = {}
+turn = 'X'
+winner = None
 
-    state['board'] = {
+def play_game():
+    global winner, turn
+    init_game()
+    while not winner:
+        print_board()
+        move = get_move()
+        board[move] = turn 
+        turn = 'O' if turn == 'X' else 'X'
+
+
+
+
+def init_game():
+    global board, turn, winner
+    board = {
         'a1':' ', 'b1':' ', 'c1': ' ',
         'a2':' ', 'b2':' ', 'c2':' ', 
         'a3':' ', 'b3':' ', 'c3':' ' 
     }
 
-    state['turn'] = {
-        1: "X",
-        -1: "O"
-    }
+    turn = "X"
+    winner = None
 
+def print_board():
     print( """
         A   B   C
         1) {} | {} | {}
@@ -23,29 +38,20 @@ def init_game():
         2) {} | {} | {}
         ---------------
         3) {} | {} | {}
-        """.format(state['board']['a1'], 
-        state['board']['b1'], state['board']['c1'], 
-        state['board']['a2'], state['board']['b2'], 
-        state['board']['c2'], state['board']['a3'],
-        state['board']['b3'], state['board']['c3']))
-    
-    player = state['turn'][1]
-    
-    player_input = input(f"Player {player}'s Move (example B2): ").lower()
-    input_validated = False
-    while( input_validated == False ):
-        if len(player_input) > 2:
-            print("Bogus move! Try again...")
-            player_input = input(f"Player {player}'s Move (example B2): ").lower()
-        if player_input[0] not in "abc":
-            print("Bogus move! Try again...")
-            player_input = input(f"Player {player}'s Move (example B2): ").lower()
-        if player_input[1] not in "123":
-            print("Bogus move! Try again...")
-            player_input = input(f"Player {player}'s Move (example B2): ").lower()
-        else:
-            input_validated = True
+        """.format(
+            str(board['a1'] or ' '), str(board['b1'] or ' '), str(board['c1'] or ' '),
+            str(board['a2'] or ' '), str(board['b2'] or ' '), str(board['c2'] or ' '),
+            str(board['a3'] or ' '), str(board['b3'] or ' '), str(board['c3'] or ' ')
+            )
+        )    
 
+def get_move():
+  while True:
+    move = input(f"Player {turn}'s Move (example B2): ").lower()
+    if move in board and board[move] ==' ':
+      return move
+    else:
+      print("Bogus move! Try again...\n")
 
-init_game()
+play_game()
 
